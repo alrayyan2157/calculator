3ca7dca8b96f5ae0c1fb935d72f74d1b6f8f64a3
alrayyan2157 <alrayyan2157@icloud.com>
2026-05-23 11:09:23 +0530

    Declaring Operate function

--- script.js
+++ script.js
@@ -18,3 +18,16 @@ function divide(x,y){
 var operand1;
 var operand2;
 var operator;
+var result = 0;
+
+function operate(operand1, operator, operand2){
+    if (operator == "+") {
+        result = add(operand1, operand2);
+    } else if (operator == "-") {
+        result = subtract(operand1, operand2);
+    } else if (operator == "*") {
+        result = multiply(operand1, operand2);
+    } else if (operator == "/") {
+        result = divide(operand1, operand2);
+    }
+}
